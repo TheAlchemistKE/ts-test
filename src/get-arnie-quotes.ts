214d4f050cf76836deb44ba6667c7de0b2a1e284
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2022-10-31 23:22:25 +0300

    Solve test.

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -3,8 +3,8 @@ import { httpGet } from './mock-http-interface';
 type TResult = any;
 
 export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  const results = await Promise.all(urls.map(url => httpGet(url)));
-  return results.map(result => {
+  const responses = await Promise.all(urls.map(url => httpGet(url)));
+  return responses.map(result => {
     if (result.status === 200) {
       return { 'Arnie Quote': JSON.parse(result.body).message };
     }
